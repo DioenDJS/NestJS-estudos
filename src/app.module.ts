384689e7a1b8183d1948ca5ee890c8256369c4ec
DioenDJS <negociospacheti@gmail.com>
2021-10-09 14:47:10 -0300

    :art: add config code first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({}),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
   ],
   providers: [AppService],
 })
