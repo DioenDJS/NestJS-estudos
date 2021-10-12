9f67c4f0f8fc75025ea748386beccf7767e445dd
DioenDJS <negociospacheti@gmail.com>
2021-10-12 14:54:49 -0300

    :art: Create UserService and add method createUser

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+    ){}
+
+  async createUser(data: CreateUserInput):Promise<User> {
+    const user = this.userRepository.create(data);
+    const userSaved = await this.userRepository.save(user);
+
+    if(! userSaved) {
+      throw new InternalServerErrorException('Problema para gerar um usuário !');
+    }
+
+    return userSaved;
+  }
+
+}
