f717ab35e3f4ba47881fe30fd4c9fda268d41def
DioenDJS <negociospacheti@gmail.com>
2021-10-12 19:43:07 -0300

    :art: Create module users

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NestjsQueryGraphQLModule, PagingStrategies } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { UserDTO } from './dto/user.dto';
+import { UpdateUserInput } from './dto/update-user.input';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Module({
-  providers: [UsersResolver, UsersService]
+  providers: [UsersResolver, UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([User])],
+      services: [UsersService],
+      resolvers: [
+        {
+          DTOClass: UserDTO,
+          EntityClass: User,
+          CreateDTOClass: CreateUserInput,
+          UpdateDTOClass: UpdateUserInput,
+          pagingStrategy: PagingStrategies.OFFSET,
+          ServiceClass: UsersService,
+        },
+      ],
+    }),
+  ],
 })
 export class UsersModule {}
